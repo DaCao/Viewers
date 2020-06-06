f98470d725114218a6b2b8dced0f4328df86d0ed
cda <cda@infervision.com>
2020-06-06 22:13:42 +0900

    working now with google chrome cors extention installed;

--- platform/viewer/public/config/default.js
+++ platform/viewer/public/config/default.js
@@ -20,6 +20,7 @@ window.config = {
         enableStudyLazyLoad: true,
         supportsFuzzyMatching: true,
         requestOptions: {
+          requestFromBrowser: true,
           auth: 'pacs:pacs',
         },
       },
