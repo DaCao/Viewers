2a6fce32641882f204a95c35a2a3ba47aee03359
cda <cda@infervision.com>
2020-06-06 15:57:34 +0900

    1. hard-coded study info; 2. remove logo from studylist;

--- platform/core/src/studies/services/qido/studies.js
+++ platform/core/src/studies/services/qido/studies.js
@@ -74,64 +74,83 @@ function getQIDOQueryParams(filter, serverSupportsQIDOIncludeField) {
   return params;
 }
 
-/**
- * Parses resulting data from a QIDO call into a set of Study MetaData
- *
- * @param resultData
- * @returns {Array} An array of Study MetaData objects
- */
-function resultDataToStudies(resultData) {
-  const studies = [];
+// /**
+//  * Parses resulting data from a QIDO call into a set of Study MetaData
+//  *
+//  * @param resultData
+//  * @returns {Array} An array of Study MetaData objects
+//  */
+// function resultDataToStudies(resultData) {
+//   const studies = [];
 
-  if (!resultData || !resultData.length) return;
+//   if (!resultData || !resultData.length) return;
 
-  resultData.forEach(study =>
-    studies.push({
-      StudyInstanceUID: DICOMWeb.getString(study['0020000D']),
-      // 00080005 = SpecificCharacterSet
-      StudyDate: DICOMWeb.getString(study['00080020']),
-      StudyTime: DICOMWeb.getString(study['00080030']),
-      AccessionNumber: DICOMWeb.getString(study['00080050']),
-      referringPhysicianName: DICOMWeb.getString(study['00080090']),
-      // 00081190 = URL
-      PatientName: DICOMWeb.getName(study['00100010']),
-      PatientID: DICOMWeb.getString(study['00100020']),
-      PatientBirthdate: DICOMWeb.getString(study['00100030']),
-      patientSex: DICOMWeb.getString(study['00100040']),
-      studyId: DICOMWeb.getString(study['00200010']),
-      numberOfStudyRelatedSeries: DICOMWeb.getString(study['00201206']),
-      numberOfStudyRelatedInstances: DICOMWeb.getString(study['00201208']),
-      StudyDescription: DICOMWeb.getString(study['00081030']),
-      // Modality: DICOMWeb.getString(study['00080060']),
-      // ModalitiesInStudy: DICOMWeb.getString(study['00080061']),
-      modalities: DICOMWeb.getString(
-        DICOMWeb.getModalities(study['00080060'], study['00080061'])
-      ),
-    })
-  );
+//   resultData.forEach(study =>
+//     studies.push({
+//       StudyInstanceUID: DICOMWeb.getString(study['0020000D']),
+//       // 00080005 = SpecificCharacterSet
+//       StudyDate: DICOMWeb.getString(study['00080020']),
+//       StudyTime: DICOMWeb.getString(study['00080030']),
+//       AccessionNumber: DICOMWeb.getString(study['00080050']),
+//       referringPhysicianName: DICOMWeb.getString(study['00080090']),
+//       // 00081190 = URL
+//       PatientName: DICOMWeb.getName(study['00100010']),
+//       PatientID: DICOMWeb.getString(study['00100020']),
+//       PatientBirthdate: DICOMWeb.getString(study['00100030']),
+//       patientSex: DICOMWeb.getString(study['00100040']),
+//       studyId: DICOMWeb.getString(study['00200010']),
+//       numberOfStudyRelatedSeries: DICOMWeb.getString(study['00201206']),
+//       numberOfStudyRelatedInstances: DICOMWeb.getString(study['00201208']),
+//       StudyDescription: DICOMWeb.getString(study['00081030']),
+//       // Modality: DICOMWeb.getString(study['00080060']),
+//       // ModalitiesInStudy: DICOMWeb.getString(study['00080061']),
+//       modalities: DICOMWeb.getString(
+//         DICOMWeb.getModalities(study['00080060'], study['00080061'])
+//       ),
+//     })
+//   );
 
-  return studies;
-}
+//   return studies;
+// }
 
 export default function Studies(server, filter) {
-  const config = {
-    url: server.qidoRoot,
-    headers: DICOMWeb.getAuthorizationHeader(server),
-    errorInterceptor: errorHandler.getHTTPErrorHandler(),
-  };
 
-  const dicomWeb = new api.DICOMwebClient(config);
-  server.qidoSupportsIncludeField =
-    server.qidoSupportsIncludeField === undefined
-      ? true
-      : server.qidoSupportsIncludeField;
-  const queryParams = getQIDOQueryParams(
-    filter,
-    server.qidoSupportsIncludeField
-  );
-  const options = {
-    queryParams,
-  };
+  // const config = {
+  //   url: server.qidoRoot,
+  //   headers: DICOMWeb.getAuthorizationHeader(server),
+  //   errorInterceptor: errorHandler.getHTTPErrorHandler(),
+  // };
 
-  return dicomWeb.searchForStudies(options).then(resultDataToStudies);
+  // const dicomWeb = new api.DICOMwebClient(config);
+  // server.qidoSupportsIncludeField =
+  //   server.qidoSupportsIncludeField === undefined
+  //     ? true
+  //     : server.qidoSupportsIncludeField;
+  // const queryParams = getQIDOQueryParams(
+  //   filter,
+  //   server.qidoSupportsIncludeField
+  // );
+  // const options = {
+  //   queryParams,
+  // };
+
+  const studies = [];
+
+  studies.push({
+    StudyInstanceUID: "1.2.840.113619.2.5.1762583153.215519.978957063.78",
+    StudyDate: "20010108",
+    StudyTime: "120022",
+    AccessionNumber: "001",
+    PatientName: "MISTER^MR",
+    PatientID: "123456",
+    PatientBirthdate: "19880101",
+    patientSex: "MALE",
+    studyId: "1.2.3.4.5.6",
+    numberOfStudyRelatedSeries: 1,
+    numberOfStudyRelatedInstances: 1,
+    StudyDescription: 1,
+    modalities: "CR",
+  });
+
+  return studies;
 }
